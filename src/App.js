1d31deeff60c0491e5f6df65b55ee792f28ea4a3
OlloClark <ollo42@hotmail.co.uk>
2023-01-30 16:32:10 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Stuffware Warehouse. Everything is under control soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.amazon.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pretty sure this link redirects to our store
         </a>
       </header>
     </div>
