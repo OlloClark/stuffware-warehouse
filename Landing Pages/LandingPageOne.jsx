3bbed2e3598e58438073e5dcdf7c640e0f9fca3d
OlloClark <ollo42@hotmail.co.uk>
2023-02-07 15:52:23 -0800

    adding LandingPageOne

---
+++ Landing Pages/LandingPageOne.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function LandingPageOne() {
+	return(
+		<div className="LandingPageOneContainer">
+			<h1>
+				Stuffware Warehouse is getting a fresh coat of paint!
+			</h1>
+			<h4>
+				Hi there Stuffware family! We have some exciting news! With the money Mason got from his mom (Rest in Peace, Abby)
+				we've decided to give the site what Erica calls "a glow up"! Thank you for your patience as we get the website
+				up and running. We've hired two very nice people to do it for us, who told us they come highly recommended.
+			</h4>
+			<h4>
+				Thank you Ollo and Kayla for the very clever computer hooplah you're doing for us! Can't wait to see the result!
+			</h4>
+			<a href="www.amazon.com">
+				Kayla said this link means we can keep the site running while it's under construction!
+			</a>
+		</div>
+	)
+}
+
+export default LandingPageOne
