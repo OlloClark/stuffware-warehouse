0eec0cb20ede084cd206399dff4fb900ea3de4e0
Kayla Kantola <krkantola@gmail.com>
2023-02-07 18:51:26 -0500

    Use new under construction loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import {LoadRandomUnderConstructionPage} from './components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Stuffware Warehouse. Everything is under control soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.amazon.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pretty sure this link redirects to our store
-        </a>
-      </header>
+      <LoadRandomUnderConstructionPage />
     </div>
   );
 }
