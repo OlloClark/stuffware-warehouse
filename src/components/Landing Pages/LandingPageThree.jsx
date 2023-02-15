bb278aec4a34dfdeaa2465b5f258b30c151f017d
OlloClark <ollo42@hotmail.co.uk>
2023-02-14 16:14:12 -0800

    basic lp3 divs laid out

--- src/components/Landing Pages/LandingPageThree.jsx
+++ src/components/Landing Pages/LandingPageThree.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 
 function LandingPageThree() {
 	return(
-		<div className="LandingPageOneContainer">
-			<h1>
-				Landing Page 3!
-			</h1>
-			<h4>
-				Hi there Stuffware family! We have some exciting news! With the money Mason got from his mom (Rest in Peace, Abby)
-				we've decided to give the site what Erica calls "a glow up"! Thank you for your patience as we get the website
-				up and running. We've hired two very nice people to do it for us, who told us they come highly recommended.
-			</h4>
-			<h4>
-				Thank you Ollo and Kayla for the very clever computer hooplah you're doing for us! Can't wait to see the result!
-			</h4>
-			<a
-			 href="https://www.amazon.com"
-			 target="_blank"
-			>
-				Kayla said this link means we can keep the site running while it's under construction!
-			</a>
+		<div className="LandingPageOneContainer grid h-screen place-items-center min-h-screen bg-gradient-to-r from-rose-900 to-rose-600">
+			<div className='border-4 border-stone-600 p-3'>
+				<h1>
+					Stuffware Warehouse
+				</h1>
+			</div>
+			<div className='border-4 border-stone-600 w-3/5 p-3'>
+				<h4>
+					Hi there Stuffware family! We have some exciting news! With the money Mason got from the bakesale
+					we've decided to give the site what Erica calls "a glow up"! Thank you for your patience as we get the website
+					up and running. We've hired two very nice people to do it for us, who told us they come highly recommended.
+				</h4>
+				<h4>
+					Thank you Ollo and Kayla for the very clever computer hooplah you're doing for us! Can't wait to see the result!
+				</h4>
+			</div>
+			<div className='border-4 border-stone-600 p-3'>
+				<a
+				href="https://www.amazon.com"
+				target="_blank"
+				>
+					Kayla said this link means we can keep the site running while it's under construction!
+				</a>
+			</div>
+			
 		</div>
 	)
 }
